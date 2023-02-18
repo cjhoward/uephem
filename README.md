f27359138e33f6a8f6cd7b560a51d0858f24f710
C. J. Howard <cjhoward@cjhoward.org>
2023-02-18 12:13:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# μephem
+
+μephem is a tiny ephemeris utility capable of generating orbital state vectors for the major bodies in our solar system. The μephem source is a single C-file with a few hundred lines of code, making it easily ported to other languages or modified to run on embedded systems.
+
+## Prerequisites
+
+A JPL Planetary and Lunar Ephemerides binary ephemeris file is required. For an overview of the available ephemerides, see <https://ssd.jpl.nasa.gov/planets/eph_export.html>. Little-endian ephemeris files can be downloaded from <ftp://ssd.jpl.nasa.gov/pub/eph/planets/Linux>. Big-endian ephemeris files can be downloaded from <ftp://ssd.jpl.nasa.gov/pub/eph/planets/SunOS>. μephem is capable of reading both little and big-endian files, regardless of the host endianness. `.bsp` ephemeris files are **not** supported.
+
+## Usage
+
+	uephem <file> <item ID> <t0> [<t1> <resolution>]
+
+### Items
+
+| ID | Name                          | Output                  | Units   | Center |
+|---:|:------------------------------|:------------------------|:--------|:------:|
+|  0 | Mercury                       | x,y,z,vx,vy,vz          | km,km/d | SSB    |
+|  1 | Venus                         | x,y,z,vx,vy,vz          | km,km/d | SSB    |
+|  2 | Earth-Moon barycenter         | x,y,z,vx,vy,vz          | km,km/d | SSB    |
+|  3 | Mars                          | x,y,z,vx,vy,vz          | km,km/d | SSB    |
+|  4 | Jupiter                       | x,y,z,vx,vy,vz          | km,km/d | SSB    |
+|  5 | Saturn                        | x,y,z,vx,vy,vz          | km,km/d | SSB    |
+|  6 | Uranus                        | x,y,z,vx,vy,vz          | km,km/d | SSB    |
+|  7 | Neptune                       | x,y,z,vx,vy,vz          | km,km/d | SSB    |
+|  8 | Pluto                         | x,y,z,vx,vy,vz          | km,km/d | SSB    |
+|  9 | Moon                          | x,y,z,vx,vy,vz          | km,km/d | Earth  |
+| 10 | Sun                           | x,y,z,vx,vy,vz          | km,km/d | SSB    |
+| 11 | Earth nutation                | d(psi),d(epsilon)       | rad     | Earth  |
+| 12 | Lunar mantle libration        | phi,theta,psi           | rad     | Moon   |
+| 13 | Lunar mantle angular velocity | omega_x,omega_y,omega_z | rad/d   | Moon   |
+| 14 | TT-TDB                        | t                       | s       | Earth  |
+
+Note that items 11-14 may not be present in some ephemeris files.
+
+## File Format
+
+JPL ephemeris binary files consist of a series fixed-size records. Record size can vary between ephemeris files. A file's record size can be inferred through its file size along with the time information given in its header.
+
+The first record in a file contains the header, the structure of which is described below:
+
+	typedef struct
+	{
+		// Comment text.
+		char comments[3][84];
+		
+		// First set of constant name labels.
+		char cnames1[400][6];
+		
+		// JD start, JD end, record duration.
+		double time[3];
+		
+		// Number of constants in the second record.
+		int32_t nconst;
+		
+		// Constant values AU and EMRAT.
+		double constants[2];
+		
+		// Record offset, coeffs per component, and coeff sets per record, for items 0-11.
+		int32_t table1[12][3];
+		
+		// DE version number.
+		int32_t denum;
+		
+		// Continuation of table1 for item 12.
+		int32_t table2[3];
+		
+		// Second set of constant name labels.
+		char cnames2[nconst - 400][6];
+		
+		// Continuation of table2 for items 13 and 14.
+		int32_t table3[2][3];
+		
+	} de_header;
+
+Note that some ephemeris files store additional constant names in the header between `table2` and `table3` if the limit of 400 constant names has been reached.
+
+The second record is simply an array of `double` values for the constants named in the header. Each of the remaining records contain two `double` values: the Julian date start and end times of the record, followed by an array of `double` Chebyshev polynomial coefficients for each item in the ephemeris.
+
+## License
+
+μephem is licensed under the MIT license. See [LICENSE.md](./LICENSE.md) for details.
---
+++ uephem.c
@@ -0,0 +1,285 @@
+/*
+ * Copyright (C) 2022 Christopher J. Howard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+#include <limits.h>
+
+#define DE_OFFSET_TIME 0xA5C
+#define DE_OFFSET_TABLE1 0xA88
+#define DE_OFFSET_DENUM 0xB18
+#define DE_OFFSET_TABLE2 0xB1C
+#define DE_ENDIAN_SWAP(denum) (denum&0xFFFF0000L)
+#define DE_MIN_ITEM_ID 0
+#define DE_MAX_ITEM_ID 14
+#define DE_MAX_NCONST 400
+#define DE_CNAME_LENGTH 6
+
+/// Number of components for items 0-14.
+const int DE_NCOMP[15] = {3,3,3,3,3,3,3,3,3,3,3,2,3,3,1};
+
+const char* err_fopen = "file open failed";
+const char* err_fread = "file read failed";
+const char* err_bad_arg = "bad argument";
+const char* err_no_item = "item not found";
+const char* err_date = "date out of range";
+
+/// Prints an error string and exits.
+void error(const char* msg)
+{	
+	printf("error: %s\n", msg);
+	exit(EXIT_FAILURE);
+}
+
+/// Swaps the byte order of a 32-bit number.
+uint32_t swap32(uint32_t x)
+{
+	x = ((x << 8) & 0xFF00FF00) | ((x >> 8) & 0xFF00FF);
+	return (x << 16) | (x >> 16);
+}
+
+/// Swaps the byte order of a 64-bit number.
+uint64_t swap64(uint64_t x)
+{
+	x = ((x << 8) & 0xFF00FF00FF00FF00) | ((x >> 8) & 0x00FF00FF00FF00FF);
+	x = ((x << 16) & 0xFFFF0000FFFF0000) | ((x >> 16) & 0x0000FFFF0000FFFF);
+	return (x << 32) | (x >> 32);
+}
+
+/// Calls fread then swaps the byte order of 32 and 64-bit data.
+size_t fread_swap(void* ptr, size_t size, size_t count, FILE* stream)
+{
+	size_t status = fread(ptr, size, count, stream);
+	if (size == sizeof(uint64_t))
+		for (uint64_t* ptr64 = (uint64_t*)ptr; count; --count, ++ptr64)
+			*ptr64 = swap64(*ptr64);
+	else if (size == sizeof(uint32_t))
+		for (uint32_t* ptr32 = (uint32_t*)ptr; count; --count, ++ptr32)
+			*ptr32 = swap32(*ptr32);
+	return status;
+}
+
+/// Evaluates a Chebyshev polynomial.
+double chebyshev(const double* a, int n, double x)
+{
+	double y = *(a++);
+	y += *(a++) * x;
+	
+	double n2 = 1.0, n1 = x, n0;
+	x *= 2.0;
+	
+	for (n -= 2; n; --n, n2 = n1, n1 = n0)
+	{
+		n0 = x * n1 - n2;
+		y += *(a++) * n0;
+	}
+	
+	return y;
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 4 && argc != 6)
+	{
+		printf("usage: uephem <file> <item ID> <t0> [<t1> <resolution>]\n");
+		return EXIT_FAILURE;
+	}
+	
+	// Open ephemeris file
+	FILE* file;
+	if (fopen_s(&file, argv[1], "rb") != 0)
+		error(err_fopen);
+	
+	// Parse item ID
+	char* endptr;
+	int32_t item_id = strtol(argv[2], &endptr, 0);
+	if (*endptr != '\0')
+		error(err_bad_arg);
+	if (item_id < DE_MIN_ITEM_ID || item_id > DE_MAX_ITEM_ID)
+		error(err_no_item);
+	
+	// Parse JD start time
+	double jd_start = strtod(argv[3], &endptr);
+	if (*endptr != '\0')
+		error(err_bad_arg);
+	
+	// Init JD end time and time resolution to single time output
+	double jd_end = jd_start;
+	int resolution = 1;
+	
+	if (argc == 6)
+	{
+		// Parse JD end time
+		jd_end = strtod(argv[4], &endptr);
+		if (*endptr != '\0')
+			error(err_bad_arg);
+		
+		// Parse time resolution
+		resolution = strtol(argv[5], &endptr, 0);
+		if (*endptr != '\0' || resolution <= 0)
+			error(err_bad_arg);
+	}
+	
+	// Determine JD step size
+	double jd_step = (jd_end - jd_start) / (resolution - 1);
+	if (jd_start == jd_end)
+	{
+		jd_step = 0.0;
+		resolution = 1;
+	}
+	else if (resolution == 1)
+	{
+		jd_start = (jd_start + jd_end) * 0.5;
+		jd_step = 0.0;
+	}
+	
+	// Read DE version number
+	int32_t denum;
+	fseek(file, DE_OFFSET_DENUM, SEEK_SET);
+	fread(&denum, sizeof(int32_t), 1, file);
+	if (ferror(file))
+		error(err_fread);
+	
+	// If the DE number has any data in its most significant word, swap the byte order of following fread calls
+	size_t (*fread_ntoh)(void*, size_t, size_t, FILE*) = &fread;
+	if (DE_ENDIAN_SWAP(denum))
+		fread_ntoh = &fread_swap;
+	
+	// Read file time
+	double file_time[3];
+	fseek(file, DE_OFFSET_TIME, SEEK_SET);
+	fread_ntoh(file_time, sizeof(double), 3, file);
+	
+	// Check if time parameters are within the file's timespan
+	if (jd_start < file_time[0] || jd_start > file_time[1] ||
+		jd_end < file_time[0] || jd_end > file_time[1])
+		error(err_date);
+	
+	// Read and combine coefficient tables
+	int32_t nconst;
+	int32_t table[15][3];
+	fread_ntoh(&nconst, sizeof(int32_t), 1, file);
+	fseek(file, DE_OFFSET_TABLE1, SEEK_SET);
+	fread_ntoh(table, sizeof(int32_t), 12 * 3, file);
+	fseek(file, DE_OFFSET_TABLE2, SEEK_SET);
+	fread_ntoh(&table[12][0], sizeof(int32_t), 3, file);
+	if (nconst > DE_MAX_NCONST)
+		fseek(file, (nconst - DE_MAX_NCONST) * DE_CNAME_LENGTH, SEEK_CUR);
+	fread_ntoh(&table[13][0], sizeof(int32_t), 2 * 3, file);
+	if (ferror(file))
+		error(err_fread);
+	
+	// Check if the specified item has any coefficients
+	if (!table[item_id][2])
+		error(err_no_item);
+	
+	// Determine number of coefficients per record
+	long rec_ncoeff = 0;
+	for (int i = 0; i < 15; ++i)
+	{
+		long ncoeff = table[i][0] + table[i][1] * table[i][2] * DE_NCOMP[i] - 1;
+		if (ncoeff > rec_ncoeff)
+			rec_ncoeff = ncoeff;
+	}
+	
+	// Allocate record buffer
+	long rec_size = rec_ncoeff * sizeof(double);
+	double* rec_buf = malloc(rec_size);
+	
+	// Get item's number of components and number of coefficients per component
+	int32_t item_ncomp = DE_NCOMP[item_id];
+	int32_t item_ncoeff = table[item_id][1];
+	
+	// Seek to first coefficient record
+	fseek(file, rec_size * 2, SEEK_SET);
+	long rec_index[2] = {-1, -1};
+	
+	// Init record-skipping constants
+	const long max_rec_skip = LONG_MAX / rec_size;
+	const long max_rec_skip_size = rec_size * max_rec_skip;
+	
+	// For each time point
+	for (int i = 0; i < resolution; ++i)
+	{
+		// Calculate JD time
+		double jd = jd_start + jd_step * i;
+		
+		// Determine coefficient record containing current time
+		rec_index[1] = (long)((jd - file_time[0]) / file_time[2]);
+		
+		// If record index changed
+		if (rec_index[0] != rec_index[1])
+		{
+			// Determine number of records to skip
+			long rec_skip = rec_index[1] - rec_index[0] - 1;
+			
+			// Handle 32-bit limits when seeking through large files
+			while (rec_skip >= max_rec_skip)
+			{
+				fseek(file, max_rec_skip_size, SEEK_CUR);
+				rec_skip -= max_rec_skip;
+			}
+			
+			// Seek to record
+			if (rec_skip)
+				fseek(file, rec_size * rec_skip, SEEK_CUR);
+			
+			// Read record into buffer
+			fread_ntoh(rec_buf, sizeof(double), rec_ncoeff, file);
+			if (ferror(file))
+				error(err_fread);
+			
+			rec_index[0] = rec_index[1];
+		}
+		
+		// Get index of the subinterval for the item at the given JD
+		double subinterval_duration = file_time[2] / (double)table[item_id][2];
+		int subinterval_index = (int)((jd - rec_buf[0]) / subinterval_duration);
+		
+		// Remap JD to the Chebyshev domain [-1, 1]
+		double subinterval_start = rec_buf[0] + subinterval_index * subinterval_duration;
+		double t = (jd - subinterval_start) / subinterval_duration * 2.0 - 1.0;
+		
+		// Pointer to the first coefficient of the first property
+		const double* coeffs = rec_buf + (table[item_id][0] - 1) + subinterval_index * item_ncoeff * item_ncomp;
+		
+		// Print Julian date
+		printf("%.*f", DBL_DECIMAL_DIG, jd);
+		
+		// For each component in the item
+		for (int j = 0; j < item_ncomp; ++j, coeffs += item_ncoeff)
+		{
+			// Evaluate the Chebyshev polynomial and output the result
+			double x = chebyshev(coeffs, item_ncoeff, t);
+			printf(",%.*e", DBL_DECIMAL_DIG, x);
+		}
+		printf("\n");
+	}
+	
+	// Clean up
+	free(rec_buf);
+	fclose(file);
+	
+	return EXIT_SUCCESS;
+}
